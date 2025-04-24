2d2702c01453c22c21f2a76bb0e9b2941388022e
er2suraj <129208069+er2suraj@users.noreply.github.com>
2025-04-24 13:59:00 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css'
-
+import SurajResume from './SurajResume.pdf';
 const Home = () => {
   return (
     <div>
@@ -13,7 +13,7 @@ const Home = () => {
             <ul>
               <li className="homel nav-hover"><a href="#Homepage">Home</a></li>
               <li className="aboutl nav-hover"><a href="#About">About</a></li>
-              {/* <li className="servicel nav-hover"><a href="#Service">Service</a></li> */}
+              <li className="servicel nav-hover"><a href={SurajResume} target="_blank" rel="noopener noreferrer">Resume</a></li>
               <li className="skilll nav-hover"><a href="#Skill">Skill</a></li>
               <li className="projectl nav-hover"><a href="#Project">Project</a></li>
               <li className="contactl nav-hover"><a href="#Contact">Contact</a></li>
@@ -27,10 +27,8 @@ const Home = () => {
           <div className="about-me">
             <h2 className="line1">Hello it's me</h2>
             <h1 className="line2">Suraj Chauhan</h1>
-            <div className="line3">
-              <h3></h3>
-              <h3 className="line4">Frontend Developer</h3>
-            </div>
+            <h3 className="line4">Frontend Developer</h3>
+           
             <p>Welcome to my personal portfolio website!</p>
             <ul className="social-link">
               <li><a href="#"><i className="fa-brands fa-facebook social-logo"></i></a></li>
@@ -51,25 +49,27 @@ const Home = () => {
         </div>
       </div>
 
+    {/* About section  */}
       <div id="About">
-        <div className="About_Image_Box">
-          <div className="About_Image"></div>
-        </div>
         <div className="AboutText">
-          <h1>About <span style={{ color: 'aqua' }}>Me</span></h1>
-          <h2>Full Stack Developer!</h2>
-          <p>
+          <h1 style={{marginLeft: '2rem'}}>About Me</h1>
+          <h2 style={{marginLeft: '2rem'}}>Full Stack Developer!</h2>
+          <p style={{marginLeft: '2rem'}}>
             Hello! I'm a passionate Full Stack Developer currently in my third
             year of pursuing a Bachelor of Technology (B.Tech).
             <br />With a solid foundation in both front-end and back-end technologies,
             <br />I love creating seamless and dynamic web applications that enhance user experiences.
           </p>
-          <button id="aboutMe">More About Me</button>
+          <button style={{float: 'center',width:'100%',marginLeft: '2rem'}} id="aboutMe">More About Me</button>
+        </div>
+        <div className="About_Image_Box">
+          <div className="About_Image"></div>
         </div>
       </div>
-
+  
+      {/* skills section  */}
       <div className="skills" id="Skill">
-        <h1>My <span style={{ color: 'aqua' }}>Skills</span></h1>
+      <h1>My <span style={{ color: 'aqua', fontSize: '1.4rem' }}>Skills</span></h1>
         <div className="skill1">
           <div className="technical">
             <h2>Technical Skills</h2>
@@ -100,7 +100,7 @@ const Home = () => {
         <h1>Projects</h1>
         <ul className='project_list'>
           <li>
-            <b style={{color: 'aqua'}}>Recipe Finder Web App:</b> 
+            <b style={{color: 'aqua',fontSize: '1.4rem'}}>Recipe Finder Web App:</b> 
             <p>
               A responsive web application that allows users to
               search and discover recipes from around the world using TheMealDB API.
@@ -111,7 +111,7 @@ const Home = () => {
           </li>
 
           <li>
-            <b style={{color: 'aqua'}}>N-Queens Visualizer: </b> 
+            <b style={{color: 'aqua',fontSize: '1.4rem'}}>N-Queens Visualizer: </b> 
             <p>
               An interactive React web app that visualizes the N-
               Queens problem using step-by-step animations of the backtracking algorithm
@@ -121,7 +121,7 @@ const Home = () => {
           </li>
 
           <li>
-            <b style={{color: 'aqua'}}> Web Projects: </b> 
+            <b style={{color: 'aqua',fontSize: '1.4rem'}}> Web Projects: </b> 
             <p>
               Designed and developed portfolio website, basic pages &
               Amazon/Netflix clones.
@@ -135,8 +135,8 @@ const Home = () => {
       </div>
 
       <div className="contact" id='Contact'>
-        <p>
-          Contact number : <a href="tel:+6392227274">6392227274</a>
+        <p style={{fontSize: '1.4rem'}}>
+          Contact number : <a href="tel:+6392227274" style={{color: 'aqua',fontSize: '1.4rem'}}>6392227274</a>
         </p>
       </div>
     </div>
